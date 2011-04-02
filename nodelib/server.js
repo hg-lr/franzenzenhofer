3b444879f3a3d074aa17f9e1ac3e3b820b5a7efe
Franz Enzenhofer <f.enzenhofer@tupalo.com>
2011-04-02 20:56:35 +0200

    new version

--- nodelib/server.js
+++ nodelib/server.js
@@ -1,7 +1,57 @@
-var app = require('express').createServer();
+express = require('express');
+util = require('util');
+var app = express.createServer();
+app.use(express.static(__dirname + '/../public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
-app.get('/', function(req, res){
-  res.render('index.jade');
-});
+var n = [
+ {
+  title: 'seo',
+  href: false,
+  navpoints: [
+    {title:'link liquidity crisis', href:'http://eu.techcrunch.com/2010/07/07/startups-linking-to-your-competition-will-help-you-no-really/'},
+    {title:'lli', href:'llI'}
+  ]
+ },
+ {
+  title: 'javascript / node.js',
+  href: false,
+  navpoints: [
+     {title:'jade', href:'jade'},
+  ]
+ },
+{
+  title: 'about',
+  href: false,
+  navpoints: [
+     {title:'curriculum vitae', href:'cv'},
+  ]
+ },
+]
+
+
+
+
+//start page
+app.get('/', function(req, res){res.render('index.jade', {title: 'Franz Enzenhofer SEO', navigation: n});});
+
+//articles
+
+//jade
+app.get('/jade', function(req, res){res.render('jade.jade', {title: 'jade tutorial', navigation: n});});
+app.get('/cv', function(req, res){res.render('cv.jade', {title: 'Franz Enzenhofer Curriculum Vitae', navigation: n});});
+
+/*app.get('/img/*', function(req, res){
+
+  var file = req.params[0];
+  res.sendfile(__dirname+ '/../public/img/' + file)
+//res.send(req.body);
+  console.log(util.inspect(req))
+  console.log(__dirname+ '/../public/img/' + file);
+
+})*/
+
+
 
 app.listen(3000);
